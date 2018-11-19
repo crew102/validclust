3245146fd5ac8116c51a686751f7eccb39a48912
christopher crew baker <chriscrewbaker@gmail.com>
2018-11-19 13:02:27 -0500

    rename dunn data param

--- validclust/indices.py
+++ validclust/indices.py
@@ -5,15 +5,14 @@ def _get_clust_pairs(clusters):
     return [(i, j) for i in clusters for j in clusters if i > j]
 
 
-def dunn(dist, labels):
+def dunn(X, labels):
     clusters = set(labels)
     inter_dists = [
-        dist[np.ix_(labels == i, labels == j)].min()
+        X[np.ix_(labels == i, labels == j)].min()
         for i, j in _get_clust_pairs(clusters)
     ]
     intra_dists = [
-        dist[np.ix_(labels == i, labels == i)].max()
+        X[np.ix_(labels == i, labels == i)].max()
         for i in clusters
     ]
     return min(inter_dists) / max(intra_dists)
-
